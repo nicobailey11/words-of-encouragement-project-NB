22f887811f9b5e92a3bf55616f400e5e389edebf
nicobailey11 <nicobailey11@gmail.com>
2023-12-17 14:13:48 -0600

    Add all project files

---
+++ script.js
@@ -0,0 +1,35 @@
+/* This function "renders" some bit of text as audio to the user. */
+function renderTTS(text) {
+  var synthesis = window.speechSynthesis;
+
+  console.log("Available Voices:");
+  var voice = synthesis.getVoices().filter(function (voice) {
+    console.log(voice.name + " " + voice.lang);
+    return voice.name === 'Google UK English Female';
+  })[0];
+
+  // Create an utterance object
+  var utterance = new SpeechSynthesisUtterance(text);
+
+  // Set utterance properties
+  utterance.voice = voice;
+  utterance.pitch = 1;
+  utterance.rate = 1;
+  utterance.volume = 1;
+
+  // Speak the utterance
+  synthesis.speak(utterance);
+}
+
+function encourage() {
+  // Get the name and words of encouragement.
+  let name = document.getElementById("name").value;
+  let encouragement = document.getElementById("encouragement").value;
+  let postScript = document.getElementById("post-script").value;
+
+  // Combine those into some text.
+  let message = `Hello ${name}, I have something I would like to tell you. ${encouragement}. ${postScript}`;
+
+  // Render text to user as speech.
+  renderTTS(message);
+}
